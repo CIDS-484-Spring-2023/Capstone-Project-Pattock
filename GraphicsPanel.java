c496917b69a864777063027ac1f360f7d42492bd
1JP123 <98541743+1JP123@users.noreply.github.com>
2023-02-10 11:57:18 -0600

    Forgot Extends JPanel

--- GraphicsPanel.java
+++ GraphicsPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 //this class is where all the changes to the board will occur, including the flag marking, showing nubmbers and showing bombs upon clicking one.
-public class GraphicsPanel 
+public class GraphicsPanel extends JPanel
 {
 
     public GraphicsPanel()
